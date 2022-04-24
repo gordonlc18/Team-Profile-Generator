54b44a4083d734312199fcd93a01cb8ed9be2a6f
gordonlc18 <gordonlc18@gmail.com>
2022-04-24 13:30:49 -0400

    Complete html template and stylesheet

--- src/template.js
+++ src/template.js
@@ -89,7 +89,7 @@ const generateTeam = (team) => {
       <meta charset="UTF-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-      <title>My Team</title>
+      <title>Team Profile</title>
       <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
           integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
       <link rel="stylesheet" href="style.css">
@@ -98,8 +98,8 @@ const generateTeam = (team) => {
   <body>
       <div class="container-fluid">
           <div class="row">
-              <div class="col-12 jumbotron mb-3 team-heading">
-                  <h1 class="text-center">My Team</h1>
+              <div class="col-12 jumbotron mb-3">
+                  <h1 class="text-center">Team Profile</h1>
               </div>
           </div>
       </div>
